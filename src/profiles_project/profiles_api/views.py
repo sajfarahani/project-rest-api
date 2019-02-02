4c968f8feb0bead1886bde157dbd7fa3a4a0b85b
sajfrahani <sajjfarahani@outlook.com>
2019-02-02 13:30:22 +1100

    Added our viewsets

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
+from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -51,7 +52,18 @@ class HelloApiView(APIView):
 
         return Response({'method': 'delete'})
 
-    
+class HelloViewSet(viewsets.ViewSet):
+    """Test API ViewSets"""
 
+    def list(self, request):
+        """Return a hello message"""
+
+        a_viewset = [
+            'Use actions (list, create, retrieve, update, patial_update)',
+            'Automatically mps to URLs using Routers',
+            'Provids more functionality with less code'
+        ]
+
+        return Response({'message': 'Hello!', 'a_viewset': a_viewset})
 
 
